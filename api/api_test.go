ed425b4f189c6dedffa196a14da601c9286528ca
Frank Schroeder <frank.schroeder@gmail.com>
2017-05-12 15:41:13 +0200

    test: add helper for ioutil.TempDir/TempFile
    This creates a simplified helper for temporary directories and files.
    All path names are prefixed with the name of the current test.
    All files and directories are stored either in /tmp/consul-test
    or /tmp if the former could not be created.
    
    Using the system temp dir breaks some tests on macOS where the unix
    socket path becomes too long.

--- api/api_test.go
+++ api/api_test.go
@@ -4,7 +4,6 @@ import (
 	crand "crypto/rand"
 	"crypto/tls"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -481,10 +480,7 @@ func TestAPI_UnixSocket(t *testing.T) {
 		t.SkipNow()
 	}
 
-	tempDir, err := ioutil.TempDir("", t.Name()+"-consul")
-	if err != nil {
-		t.Fatalf("err: %s", err)
-	}
+	tempDir := testutil.TempDir(t, "consul")
 	defer os.RemoveAll(tempDir)
 	socket := filepath.Join(tempDir, "test.sock")
 
